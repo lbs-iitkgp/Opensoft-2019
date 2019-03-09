32be4676ae7d13112d01420fedaeddde973886ea
Athitya <athityakumar@gmail.com>
2019-03-10 03:46:49 +0530

    Adds pycodestyle for linting, and lints corpus fetching script

--- api/fetch_word_corpus.py
+++ api/fetch_word_corpus.py
@@ -4,31 +4,35 @@ from env import ENV
 CHARS_TO_REMOVE = list('''.()"',-:;''')
 CORPUS = dict()
 
+
 def sanitize_line(line):
-	for char_to_remove in CHARS_TO_REMOVE:
-		line = line.replace(char_to_remove, ' ')
-	return(line)
+    for char_to_remove in CHARS_TO_REMOVE:
+        line = line.replace(char_to_remove, ' ')
+    return(line)
+
 
 def fetch_corpus_from_file(filename):
-	if filename.endswith(".txt"):
-		with open("{}/CaseDocuments/All_FT/{}".format(ENV["DATASET_PATH"], case_filename)) as f:
-			for line in f.readlines():
-				line = sanitize_line(line)
-				words = line.lower().split()
-				for word in words:
-					word = word.strip()
-					if len(word) > 3:
-						if word in CORPUS:
-							CORPUS[word] += 1
-						else:
-							CORPUS[word] = 1
+    if filename.endswith(".txt"):
+        with open("{}/CaseDocuments/All_FT/{}".format(ENV["DATASET_PATH"], case_filename)) as f:
+            for line in f.readlines():
+                line = sanitize_line(line)
+                words = line.lower().split()
+                for word in words:
+                    word = word.strip()
+                    if len(word) > 3:
+                        if word in CORPUS:
+                            CORPUS[word] += 1
+                        else:
+                            CORPUS[word] = 1
+
 
 def fetch_corpus():
-	for filename in os.listdir("{}/CaseDocuments/All_FT".format(ENV["DATASET_PATH"])):
-		fetch_corpus_from_file(filename)
+    for filename in os.listdir("{}/CaseDocuments/All_FT".format(ENV["DATASET_PATH"])):
+        fetch_corpus_from_file(filename)
+
 
 if __name__ == "__main__":
-	fetch_corpus()
-	for (word, count) in sorted(CORPUS.items(), key=lambda k : k[1], reverse=True):
-		print(word, count)
-	print(len(CORPUS))
+    fetch_corpus()
+    for (word, count) in sorted(CORPUS.items(), key=lambda k: k[1], reverse=True):
+        print(word, count)
+    print(len(CORPUS))
