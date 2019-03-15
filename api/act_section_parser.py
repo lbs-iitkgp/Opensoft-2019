d92ed6ef5f2af9b4d7d77bf51efb29a3298d8117
thebhatman <manjunathbhat9920@gmail.com>
2019-03-16 05:08:20 +0530

    Stuff like u/s., s. replaced by Section

--- api/act_section_parser.py
+++ api/act_section_parser.py
@@ -9,18 +9,26 @@ all_cases = filter(lambda x: x[-4:] == ".txt", all_files)
 nlp = spacy.load('en_core_web_sm')
 
 case_dict = {}
-for i in range(len(all_cases)):
-	file = open("./All_FT/" + all_cases[i], 'r')
+for j in range(200):
+	file = open("./All_FT/" + all_cases[j], 'r')
 	text = ""
 	line_num = 1
-	#print(all_cases[i])
-	case_dict[all_cases[i]] = {}
+	print(all_cases[j])
+	case_dict[all_cases[j]] = {}
 	prev_act_name = ""
 	for line in file:
+		if "u/s." in line :
+			line = line.replace("u/s.", "Section")
+		if "s." in line:
+			line = line.replace("s.", "Section")
+		if "S." in line:
+			line = line.replace("S.", "Section")
+		if "section" in line:
+			line = line.replace("section", "Section")
+
 		text += line
 		doc = nlp(text)
 		line_words = line.split(" ")
-
 		for entity in doc.ents:
 			act_bool = 0
 			words = entity.text.split(" ")
@@ -42,21 +50,16 @@ for i in range(len(all_cases)):
 					
 					sect_flag = 0
 					sect_num = -1
-
 					if parts[0] == "Section" or parts[0] == "S." or parts[0] == "section" or parts[0] == "s." or parts[0] == "u/s." or parts[0] == "Article":
-						
 						i = 0
 						sect_flag = 1
 						sect_num = parts[1]
-						
 						while parts[i] != "the":
 							i = i + 1
 							if i >= len(parts):
 								break
-
 						parts = parts[i + 1:]
 						act_name = ""
-						
 						for part in parts:
 							if act_name == "":
 								act_name = act_name + part
@@ -66,20 +69,18 @@ for i in range(len(all_cases)):
 					if len(parts) <= 1:
 						continue
 
-					if act_name != "" and act_name not in case_dict[all_cases[i]]:
+					if act_name != "" and act_name not in case_dict[all_cases[j]]:
 						prev_act_name = act_name
-						case_dict[all_cases[i]][prev_act_name] = set()
-					
+						case_dict[all_cases[j]][prev_act_name] = []
 					if sect_flag and prev_act_name != "":
-						case_dict[all_cases[i]][prev_act_name].add(sect_num)
-					#print(prev_act_name)
+						case_dict[all_cases[j]][prev_act_name].append(sect_num)
+					print(prev_act_name)
 					if parts[0] in line_words:
 						ind = line_words.index(parts[0])
-						
 						while ind > 0:
 							if line_words[ind] == "u/s." or line_words[ind] == "s." or line_words[ind] == "section" or line_words[ind] == "Section" or line_words[ind] == "S.":
 								sect_num = line_words[ind + 1]
-								case_dict[all_cases[i]][prev_act_name].add(sect_num)
+								case_dict[all_cases[j]][prev_act_name].append(sect_num)
 								break
 							ind = ind - 1
 	
