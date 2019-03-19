e9d64e425a4da5fb89b34e0353c12650ab87bf5e
vinay612 <vinayagarwal612@gmail.com>
2019-03-19 00:40:27 +0000

    Reading Graph from NEO4J

--- api/base_class/py2neos.py
+++ api/base_class/py2neos.py
@@ -4,7 +4,7 @@ from graph_io import import_graph
 from subgraph import graph_query
 
 # replace 'foobar' with your password
-authenticate("localhost:7474", "neo4j", "Vinay@21")
+authenticate("localhost:7474", "neo4j", "12345")
 graph = Graph()
 
 # a = Node("Person", name="Alice")
@@ -12,31 +12,31 @@ graph = Graph()
 # print(graph)
 string_to_instance=dict()
 
-nx_graph = import_graph('LegalKnowledgeGraph.json')
-nx_graph_2 = graph_query(nx_graph, judges=["Hans Raj Khanna J."], subjects=[], keywords=[], judgements=[], types=[], year_range=[])
+#nx_graph = import_graph('LegalKnowledgeGraph.json')
+#nx_graph_2 = graph_query(nx_graph, judges=["Hans Raj Khanna J."], subjects=[], keywords=[], judgements=[], types=[], year_range=[])
 
-data = nx_graph_2.nodes(data=True)
-for n in nx_graph_2:
-    print(data[n]["type"], n)
-    a = Node(data[n]["type"], id=n)
-    # print(n)
-    string_to_instance[n] = a
+#data = nx_graph_2.nodes(data=True)
+#for n in nx_graph_2:
+#    print(data[n]["type"], n)
+#    a = Node(data[n]["type"], id=n)
+#    # print(n)
+#    string_to_instance[n] = a
 
-for n in nx_graph_2:
-    for neigh in nx_graph_2[n]:
-        print(n, neigh, n in string_to_instance, neigh in string_to_instance)
-        b = Relationship(string_to_instance[n], "MAPS TO", string_to_instance[neigh])
-        graph.create(b)
+#for n in nx_graph_2:
+#    for neigh in nx_graph_2[n]:
+#        print(n, neigh, n in string_to_instance, neigh in string_to_instance)
+#        b = Relationship(string_to_instance[n], "MAPS TO", string_to_instance[neigh])
+#        graph.create(b)
 
-# with open('LegalKnowledgeGraph.json','r') as data_file:
-#     data = json.load(data_file)
-#     for node in data["nodes"]:
-#         a = Node(node["type"], id=node["id"])
-#         string_to_instance[node["id"]]=a
-# #      graph.create(a)
-#     for link in data["links"]:
-#         b = Relationship(string_to_instance[link["source"]],"MAPS TO",string_to_instance[link["target"]])
-#         graph.create(b)
+with open('LegalKnowledgeGraph.json','r') as data_file:
+        data = json.load(data_file)
+        for node in data["nodes"]:
+                a = Node(node["type"], id=node["id"])
+                string_to_instance[node["id"]]=a
+ #              graph.create(a)
+        for link in data["links"]:
+                b = Relationship(string_to_instance[link["source"]],"MAPS TO",string_to_instance[link["target"]])
+                graph.create(b)
 #for record in graph.cypher.execute("MATCH (c:case) RETURN c.id AS id"):
 # print(record.id)
 #results = graph.cypher.execute("MATCH (n:Person) RETURN n")
