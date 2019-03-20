1af9109eb6bbec867fd252b7e85ab8917efbccaa
Shankusu7 <pankaj08072000@gmail.com>
2019-03-21 05:23:57 +0530

    modified(modularised) to work with db_impoter.

--- api/acts_separated.py
+++ api/acts_separated.py
@@ -21,63 +21,69 @@ DICTIONARY_OF_ACTS = {}
 '''
     Finds the acts in the central acts folder
     '''
-for g in CENTRAL_FILES:
-    path = g
-    curr_files = [files for files in os.listdir(path)]
-    for filed in curr_files:
-        r = os.path.join(path, filed)
-        with open(r) as fg:
-            type_f = "Central"
-            myline = fg.readline()
-            idx = myline.find('_Section')
-            yr = myline[idx - 4:idx]
-            act = myline[:idx - 6]
 
-            if 'Act' in act:
-                act = act[:act.rindex('Act')]
-            if 'act' in act:
-                act = act[:act.rindex('act')]
-            act = act.strip()
-            sections = sum(1 for ln in fg)
-            if act == "":
-                continue
-            try:
-                DICTIONARY_OF_ACTS[act].append([yr, type_f, sections + 1])
-            except KeyError:
-                DICTIONARY_OF_ACTS[act] = []
-                DICTIONARY_OF_ACTS[act].append([yr, type_f, sections + 1])
-        # i += 1
-for g in STATE_FILES:
-    path = g
-    curr_files = [files for files in os.listdir(path)]
-    curr_act = []   
-    for filed in curr_files:
-        r = os.path.join(path, filed)
-        with open(r) as fg:
-            myline = fg.readline()
-            head, tail = os.path.split(path)
-            type_f = tail
-            idx = myline.find('_Section')
-            yr = myline[idx - 4:idx]
-            act = myline[:idx - 6]
 
-            if 'Act' in act:
-                act = act[:act.rindex('Act')]
-            if 'act' in act:
-                act = act[:act.rindex('act')]
-            act = act.strip()
-            sections = sum(1 for ln in fg)
-            if act == "":
-                continue
-            try:
-                DICTIONARY_OF_ACTS[act].append([yr, type_f, sections + 1])
-            except KeyError:
-                DICTIONARY_OF_ACTS[act] = [] 
-                DICTIONARY_OF_ACTS[act].append([yr, type_f, sections + 1])
-CNT = 0
-for key in DICTIONARY_OF_ACTS:
-    CNT += 1
-print(CNT)
+def acts_data_miner():
+    serial = 0
+    for g in CENTRAL_FILES:
+        path = g
+        curr_files = [files for files in os.listdir(path)]
+        for filed in curr_files:
+            serial += 1
+            r = os.path.join(path, filed)
+            with open(r) as fg:
+                type_f = "Central"
+                myline = fg.readline()
+                idx = myline.find('_Section')
+                yr = myline[idx - 4:idx]
+                act = myline[:idx - 6]
 
-with open('ACTS_BY_STATES.json', 'w') as ff:
-    json.dump(DICTIONARY_OF_ACTS, ff, indent=4,sort_keys=True)
+                if 'Act' in act:
+                    act = act[:act.rindex('Act')]
+                if 'act' in act:
+                    act = act[:act.rindex('act')]
+                act = act.strip()
+                sections = sum(1 for ln in fg)
+                if act == "":
+                    continue
+                try:
+                    DICTIONARY_OF_ACTS[act].append([serial, yr, type_f, filed, "Space For Page Rank Score"])
+                except KeyError:
+                    DICTIONARY_OF_ACTS[act] = []
+                    DICTIONARY_OF_ACTS[act].append([serial, yr, type_f, filed, "Space For Page Rank Score"])
+            # i += 1
+    for g in STATE_FILES:
+        path = g
+        curr_files = [files for files in os.listdir(path)]
+        curr_act = []
+        for filed in curr_files:
+            serial += 1
+            r = os.path.join(path, filed)
+            with open(r) as fg:
+                myline = fg.readline()
+                head, tail = os.path.split(path)
+                type_f = tail
+                idx = myline.find('_Section')
+                yr = myline[idx - 4:idx]
+                act = myline[:idx - 6]
+
+                if 'Act' in act:
+                    act = act[:act.rindex('Act')]
+                if 'act' in act:
+                    act = act[:act.rindex('act')]
+                act = act.strip()
+                sections = sum(1 for ln in fg)
+                if act == "":
+                    continue
+                try:
+                    DICTIONARY_OF_ACTS[act].append([serial, yr, type_f, filed, "Space For Page Rank Score"])
+                except KeyError:
+                    DICTIONARY_OF_ACTS[act] = []
+                    DICTIONARY_OF_ACTS[act].append([serial, yr, type_f, filed, "Space For Page Rank Score"])
+    return DICTIONARY_OF_ACTS
+
+
+if __name__ == "__main__":
+    acts_data_miner()
+    with open('ACTS_BY_STATES.json', 'w') as ff:
+        json.dump(DICTIONARY_OF_ACTS, ff, indent=4,sort_keys=True)
