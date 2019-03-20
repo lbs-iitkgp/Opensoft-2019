78916fe09c04a735b96f3b9f35a95eefc58b881e
Arib Alam <arib.alam.iitkgp@gmail.com>
2019-03-20 16:31:34 +0530

    create get_closest_match function
    create function to find closest match from the list of strings

--- api/string_matching.py
+++ api/string_matching.py
@@ -1,3 +1,13 @@
+def get_closest_match(query, suggestions):
+
+    suggestions = [(text, find_score(query, text)) for text in suggestions]
+    # suggestions = sorted(suggestions, key=lambda x: fuzz.token_set_ratio(query, x), reverse=True)
+    suggestions = sorted(suggestions, key=lambda x: x[1], reverse=True)
+    # return [text for text in suggestions if text[1] >= 0.9 * suggestions[0][1] and text[1] <= 1.1 * suggestions[0][1]]
+    # return [text for text in suggestions if text[1] == suggestions[0][1]]
+    return suggestions[0]
+
+  
 PATH = os.path.join('base_class', 'act.json')
 file = open(PATH, 'r')
 act_list = json.loads(file.read())
