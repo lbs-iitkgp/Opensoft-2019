9914826f0384efb3f3ebfb15d7824cc0103ee732
Arib Alam <arib.alam.iitkgp@gmail.com>
2019-03-20 16:53:26 +0530

    Update string_matching.py

--- api/string_matching.py
+++ api/string_matching.py
@@ -1,29 +1,39 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+import os
+import json
+import re
+
+
 def find_score(query, text):
+    '''
+    Finds the similarity score between the query and the given text.
+    '''
     score_without_bracket = -1
     bracket_regex = '(\(((\w|\s|\.)+)\))'
     match = re.search(bracket_regex, text)
     if match:
         score_without_bracket = fuzz.token_set_ratio(query, text.replace(match.groups()[0], ''))
-    # if query[-4:].isnumeric() and text[-4:].isnumeric():
-    #     query_year = int(query[-4:])
-    #     text_year = int(text[-4:])
-    #     return fuzz.token_set_ratio(query, text) + 100 - abs(query_year - text_year)/(2018 - 1900)*100
     return max(score_without_bracket, fuzz.token_set_ratio(query, text))
 
 
 def get_closest_match(query, suggestions):
-
+    '''
+    Finds the closest matched string given a query and a list of suggestions.
+    Sorts the suggestions list in descending order according to their similarity score with the query.
+    returns the first one from the list.
+    '''
     suggestions = [(text, find_score(query, text)) for text in suggestions]
-    # suggestions = sorted(suggestions, key=lambda x: fuzz.token_set_ratio(query, x), reverse=True)
     suggestions = sorted(suggestions, key=lambda x: x[1], reverse=True)
-    # return [text for text in suggestions if text[1] >= 0.9 * suggestions[0][1] and text[1] <= 1.1 * suggestions[0][1]]
-    # return [text for text in suggestions if text[1] == suggestions[0][1]]
     return suggestions[0]
 
-  
-PATH = os.path.join('base_class', 'act.json')
-file = open(PATH, 'r')
-act_list = json.loads(file.read())
-acts = [act_data[0] for act_data in act_list]
 
-query = input("Enter Act name : ")
+# uncomment for testing files
+# PATH = os.path.join('base_class', 'act.json')
+# file = open(PATH, 'r')
+# act_list = json.loads(file.read())
+# acts = [act_data[0] for act_data in act_list]
+
+# query = input("Enter Act name : ")
+
+# print(get_closest_match(query, acts))
