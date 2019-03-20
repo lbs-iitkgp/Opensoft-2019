6c313c066ca7c0850432be27e26512d8ea10990a
Arib Alam <arib.alam.iitkgp@gmail.com>
2019-03-20 16:37:36 +0530

    improve find_score function
    the find_score function now also strips off the brackets of the suggestion text and checks for similarity. The highest of both suggestions are taken.

--- api/string_matching.py
+++ api/string_matching.py
@@ -1,5 +1,14 @@
 def find_score(query, text):
-    return fuzz.token_set_ratio(query, text)
+    score_without_bracket = -1
+    bracket_regex = '(\(((\w|\s|\.)+)\))'
+    match = re.search(bracket_regex, text)
+    if match:
+        score_without_bracket = fuzz.token_set_ratio(query, text.replace(match.groups()[0], ''))
+    # if query[-4:].isnumeric() and text[-4:].isnumeric():
+    #     query_year = int(query[-4:])
+    #     text_year = int(text[-4:])
+    #     return fuzz.token_set_ratio(query, text) + 100 - abs(query_year - text_year)/(2018 - 1900)*100
+    return max(score_without_bracket, fuzz.token_set_ratio(query, text))
 
 
 def get_closest_match(query, suggestions):
