35195b856d349f5b1d6e06fb707584d4d19d9120
kshitij <kshitijagrawal2607@gmail.com>
2019-03-22 13:37:59 +0530

    Added script for forming judge-case nodes

--- api/successful_judge.py
+++ api/successful_judge.py
@@ -1,183 +1,309 @@
+'''
+    This module returns the graph having JUDGES and cases as nodes
+            with judge nodes pointing to their respective case nodes
+    '''
+
 import os
-import json
-import networkx as nx
-from legal_graph import LegalKnowledgeGraph
-# j = nx.DiGraph()
+import re
+import nltk
+from env import ENV
+CASE_FILE = os.listdir("{}/All_FT".format(ENV["DATASET_PATH"]))
 
-judge_names_dict_final = {}
+JUDGES = []
 
-all_files = os.listdir("./All_FT")
-all_cases = [x for x in all_files if x[-4:] == ".txt"]
+# List of Honorary title present commonly in a Judge's name
+#   and are removed for easy-handling of names
 
-# print(len(all_cases))
-judge_names = {}
-CASE_FILE_TO_ID = dict()
-judge_names_as_list = []
-judge_names_as_set = []
-case_without_judge = 0
-flag = 0
-first_line = 1
+HONORIFICS = ["MR. ", "MRS. ", "DR. ", "MRS. ",
+              "SHRI ", "SHRI ", "HON'BLE ", "JUSTICE ", "CJI ", 
+              "JUTICE ","(J)","(CJ)"]
 
 
-def judge_to_case_helper():
-    count = 0
-    for case in all_cases:
-        file = open("./All_FT/" + case, 'r')
-        flag = 0
-        first_line = 1
-        for line in file:
-            line = line[0: -1]
+def check_match(a, b):
+    '''
+        Returns if two strings a (or b) is an abbreviation of string b (or a)
+                Ex. H. R. KHANNA == HANS RAJ KHANNA
+        '''
+    l = a.split()
+    m = b.split()
 
-            if first_line:
-                title = line
-                first_line = 0
+    r = len(m)
 
-            words = line.split(" ")
+    if len(l) != r:
+        return 0
 
-            search_str1 = "Judgement"
-            search_str2 = "Judgment"
-            search_str3 = "judgement"
-            search_str4 = "judgment"
-            search_str5 = "delivered"
+    for i in range(r):
+        if l[r - i - 1] != m[r - i - 1]:
+            j = i
+            break
 
-            year = case[0:4]
+    if j == 0:  # Title didn't match hence names are different
+        return 0
 
-            if year in words and len(words) <= 4:
-                date_line = line
+    ans = 0
 
-            if (search_str1 in words or search_str2 in words or search_str3 in words or search_str4 in words) and search_str5 in words:
-                flag = 1
-                for i in range(len(words)):
-                    if words[i] == "by:" or words[i] == ":":
-                        name = ""
-                        i = i + 1
+    for k in range(r - j):
 
-                        while i < len(words):
-                            name = name + " " + words[i]
-                            i = i + 1
+        if l[k][0] != m[k][0]:
+            break
 
-                        if ':' in name:
-                            name = name[: name.index(
-                                ':')] + name[name.index(':') + 1:]
+        if k == r - j - 1:
+            ans = (l[k][0] == m[k][0])
 
-                        if ',' in name:
-                            name = name[: name.index(
-                                ',')] + name[name.index(',') + 1:]
+    return ans
 
-                        if name != "":
-                            if name in judge_names:
-                                judge_names[name].append(
-                                    {"Case": case, "Date": date_line, "Title": title})
-                            else:
-                                judge_names[name] = []
-                                judge_names[name].append(
-                                    {"Case": case, "Date": date_line, "Title": title})
-                            break
-                        elif name == "":
-                            count += 1
-                            if "judgeless" in judge_names:
-                                judge_names["judgeless"].append(
-                                    {"Case": case, "Date": date_line, "Title": title})
-                            else:
-                                judge_names["judgeless"] = []
-                                judge_names["judgeless"].append(
-                                    {"Case": case, "Date": date_line, "Title": title})
-                            break
 
-                    elif words[i] == "by":
-                        name = ""
-                        i = i + 1
+def check_typo(a, b):
+    '''
+        Marks strings a and b as same if their edit distance is at-max 1
+                and they differ only in non-abbreviated part of names
+                        Ex. P. JAGAN`M`OHAN REDDY == P. JAGAN`B`OHAN REDDY
+        '''
+    l = a.split()
+    m = b.split()
 
-                        while i < len(words):
-                            name = name + " " + words[i]
-                            i = i + 1
+    r = len(m)
 
-                        if ':' in name:
-                            name = name[: name.index(
-                                ':')] + name[name.index(':') + 1:]
+    if len(l) != r:
+        return 0
 
-                        if ',' in name:
-                            name = name[: name.index(
-                                ',')] + name[name.index(',') + 1:]
-                        name = name.strip()
-                        if name != "":
-                            if name in judge_names:
-                                judge_names[name].append(
-                                    {"Case": case, "Date": date_line, "Title": title})
-                            else:
-                                judge_names[name] = []
-                                judge_names[name].append(
-                                    {"Case": case, "Date": date_line, "Title": title})
-                            break
-                        elif name == "":
-                            count += 1
-                            if "judgeless" in judge_names:
-                                judge_names["judgeless"].append(
-                                    {"Case": case, "Date": date_line, "Title": title})
-                            else:
-                                judge_names["judgeless"] = []
-                                judge_names["judgeless"].append(
-                                    {"Case": case, "Date": date_line, "Title": title})
-                            break
+    for i in range(r):
+        if l[i] != m[i]:
+            if l[i] != m[i]:
+                if len(l[i]) == len(m[i]) and len(l[i]) == 2:
+                    return 0
+                elif len(l[i]) == len(m[i]) and len(l[i]) != 2:
+                    return 1
 
-                break
-                
-            # if name == "":
-            # 	case_without_judge += 1
-            # 	judge_names[name].append({"Case":case, "Date": date_line})
-    # print(len(judge_names))
 
-    # repeated_names = []
+PARENT = {}
+CASE_FILE_TO_ID = {}
 
-    for judge in judge_names:
-        s = set()
-        [s.add(k) for k in judge.split()]
-        if s not in judge_names_as_set:
-            judge_names_dict_final[judge] = []
-            judge_names_dict_final[judge].append(judge_names[judge])
-            judge_names_as_set.append(s)
-            judge_names_as_list.append(judge)
-        else:
-            judge_names_dict_final[judge_names_as_list[judge_names_as_set.index(s)]].append(
-                judge_names[judge])
+def get_parent(a):
+    '''
+        Returns the global parent in the child-parent tree of a name
+                Ex. HANS RAJ KHANNA --> HANS. RAJ. KHANNA --> H. R. KHANNA
+        '''
+    while a != PARENT[a]:
+        a = PARENT[a]
+    return a
 
-    with open('doc_path_ttl_id.txt') as f:
-        for line in f.readlines():
+
+def get_case_id():
+    '''
+        Returns the Indlaw SC Id for a case and its name (as in All_FT directory)
+        '''
+    with open('doc_path_ttl_id.txt') as doc_file:   
+
+        for line in doc_file.readlines():
+
             line = line.strip()
             file_name, title, case_id = line.split("-->")
             CASE_FILE_TO_ID[file_name] = case_id
-    ##case_without_judge = count
-    print(count)
 
 
-def judge_to_case(knowledge_graph=LegalKnowledgeGraph()):
+def judge_to_case(graph):
+    '''
+        This function builds the required graph having judge pointing to his/her cases
+        '''
+    get_case_id()
 
-    judge_to_case_helper()
+    temporary_judges = {}
+    immediate_next_line = {}
+    for file_name in CASE_FILE:
+        path = os.path.join('./All_FT', file_name)
 
-    for k in judge_names_as_list:
-        for diction in judge_names_dict_final[k]:
-            for x in range(len(diction)):
-                if diction[x]["Case"][:-4] in CASE_FILE_TO_ID:
-                    knowledge_graph.add_edge_judge_case(
-                        k, CASE_FILE_TO_ID[diction[x]["Case"][:-4]])
-    return knowledge_graph
+        # Get the key value for CASE_FILE_TO_ID
+        idx = re.search(r".txt", file_name)
 
-# print(len(knowledge_graph.nodes()))
+        if idx is not None:
+            idx = idx.start()
+        file_key = file_name[:idx]
+        with open("{}/All_FT/{}".format(ENV["DATASET_PATH"], file_name)) as curr_file:
+        # with open(path, 'r') as curr_file:
 
-# print(j[judge_names_as_list[0]])
+            found_judge = 0
+            found_next_line = 0
 
+            for line in curr_file.readlines():
+                if found_judge:
+                    found_next_line = 1
+                    immediate_next_line[file_key] = line
 
-# print(repeated_names)
+                idx = re.search(r"delivered", line)
 
-# non = 0
-# nono=0
-# for k in judge_names_as_list:
-# 	if 70<len(k):
-# 		nono+=1
-# 		if k.find("Hon") == -1 and k.find("knowledge_graph.") ==-1 :
-# 			non+=1
-# 			print(k)
+                if idx is not None and not found_judge:
+                    found_judge = 1
+                    idx = idx.start()
+                    if len(line) <= 150:
+                        judge_containing_line = line[idx + 9:]
+                    else:
+                        judge_containing_line = "NONE"
 
-# print(nono)
-# print(non)
+                elif not found_judge:  # Case with no judge
+                    judge_containing_line = "NONE"
 
+                if found_judge and found_next_line:
+                    temporary_judges[file_key] = judge_containing_line
+                    break
+
+    final_list = {}
+    result = {}
+    cnt = 0
+
+    print(len(temporary_judges))
+
+    for judge_key in temporary_judges:
+        cnt += 1
+
+        judge = temporary_judges[judge_key]
+
+        judge = judge.strip()
+        judge = judge.upper()
+
+        for word in HONORIFICS:
+            if word in judge:
+                judge = judge.replace(word, '')
+        if 'BY ' in judge:
+            judge = judge[:judge.index('BY ')] + judge[judge.index('BY ') + 3:]
+        if 'BY:' in judge:
+            judge = judge[:judge.index('BY:')] + judge[judge.index('BY:') + 3:]
+        if ':' in judge:
+            judge = judge[:judge.index(':')] + judge[judge.index(':') + 1:]
+
+        if 'BY' == judge[-2:]:
+            judge = immediate_next_line[judge_key]
+        # if 'BY' in judge:	# ???
+        # 	# Name probably in the next line
+        # 	idx = judge.index('BY')
+        # 	if idx == 0:
+        # 		req_line = immediate_next_line[judge_key]
+        # 		req_line = req_line.strip('1.')
+        # 		req_line = req_line.rstrip('J.')
+        # 		judge = req_line
+        # 	else:
+        # 		if not (judge[idx - 1] >= 'a' and judge[idx - 1] <= 'z'):
+        # 			req_line = immediate_next_line[judge_key]
+        
+        judge = judge.strip()
+        judge = judge.rstrip('J.')
+        judge = judge.rstrip('J')
+        judge = judge.rstrip(',')
+
+        names = re.split(',', judge)
+        multiple_names = []
+        for name in names:
+            if name.find(' AND ') != -1:
+                temp = name.split('AND ')
+                for tt in temp:
+                    multiple_names.append(tt)
+            else:
+                multiple_names.append(name)
+
+        for i in range(len(multiple_names)):
+
+            multiple_names[i] = multiple_names[i].strip()
+            multiple_names[i] = multiple_names[i].rstrip('J.')
+
+            if multiple_names[i] == '':
+                continue
+            if multiple_names[i] == ' ':
+                continue
+            string = ""
+            for character in multiple_names[i]:
+                if character == '.':
+                    string += '. '
+                else:
+                    string += character
+
+            multiple_names[i] = string
+            multiple_names[i] = multiple_names[i].strip()
+
+        multiple_names = list(filter(lambda a: a != 'J. ', multiple_names))
+        multiple_names = list(filter(lambda a: a != 'J.', multiple_names))
+        multiple_names = list(filter(lambda a: a != 'J', multiple_names))
+
+        for i in range(len(multiple_names)):
+            multiple_names[i] = " ".join(multiple_names[i].split())
+
+        for name in multiple_names:
+            if len(name) <= 2:
+                continue
+            PARENT[name] = name
+            try:
+                final_list[name].append(judge_key)
+            except KeyError:
+                final_list[name] = []
+                final_list[name].append(judge_key)
+
+    print(len(final_list))
+
+    done = set()
+
+    for key_1 in PARENT:
+        for key_2 in PARENT:
+            if key_1 != key_2:
+                m = min(key_1, key_2)
+                n = max(key_1, key_2)
+
+                if (m, n) not in done:
+                    done.add((m, n))
+                    check = check_match(n, m)
+                    if check:
+                        if n.find('.') != -1:
+                            if m.find('.') == -1:
+                                PARENT[m] = n
+                            else:
+                                i1 = n.index('.')
+                                if i1 == 1:
+                                    PARENT[m] = n
+                                else:
+                                    PARENT[n] = m
+
+    done = set()
+    for key in PARENT:
+        PARENT[key] = get_parent(key)
+
+    for key_1 in PARENT:
+        for key_2 in PARENT:
+            if key_1 != key_2:
+                m = key_1
+                n = key_2
+                if (m, n) not in done:
+                    done.add((m, n))
+                    done.add((n, m))
+                    edit_dist = nltk.edit_distance(m, n)
+                    if edit_dist <= 1 and check_typo(m, n):
+                        if PARENT[m] != n:
+                            PARENT[n] = m
+
+    done = set()
+    marked = set()
+    for key in PARENT:
+        result[PARENT[key]] = []
+    for key_1 in final_list:
+        for key_2 in final_list:
+            m = min(key_1, key_2)
+            n = max(key_1, key_2)
+
+            if (m, n) in done:
+                continue
+            if m != n:
+                done.add((m, n))
+                done.add((n, m))
+                p = PARENT[n]
+                q = PARENT[m]
+
+                if n not in marked:
+                    marked.add(n)
+                    result[p] += final_list[n]
+                if m not in marked:
+                    marked.add(m)
+                    result[q] += final_list[m]
+
+    for judge in result:
+        for key in result[judge]:
+            graph.add_edge_judge_case(judge, key)
+
+    return graph
+        
