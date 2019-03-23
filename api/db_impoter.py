e5c1b91db6551226e693453754606ba1d291dca8
Shankusu7 <pankaj08072000@gmail.com>
2019-03-23 23:21:29 +0530

    renamed the db, collections, etc. changes basic functionality also

--- api/db_impoter.py
+++ api/db_impoter.py
@@ -12,27 +12,30 @@ import json
 import pymongo
 import base64
 import acts_separated
-from encode_helper import enkode, dekode
+from encode_helper import custom_encode, custom_decode
 
 # Year, State, no.of sections ## Make serial id as key add act
 acts_ka_data = acts_separated.acts_data_miner()
 acts_ka_data_encoded = {}
-s_acts_ka_data = {}
+s_acts_ka_data = []
+
 for x in acts_ka_data:
     for phew in acts_ka_data[x]:
-        phew.append(x)
-        var = str(phew[0])
-        phew.pop(0)
-        s_acts_ka_data[var] = phew
+        # phew.append(x)
+        # var = str(phew[0])
+        # phew.pop(0)
+        s_acts_ka_data.append(phew)
 
-# print(json.dumps(s_acts_ka_data, indent = 4))
+print(json.dumps(s_acts_ka_data, indent=4))
 
 for x in s_acts_ka_data:
     # x_binari = x.encode("utf-8")
-    x = str(x)
-    print(s_acts_ka_data[x])
+    
+    
+#     print(json.dumps(s_acts_ka_data[x], indent = 2))
     # print("Yo")
-    acts_ka_data_encoded[enkode(x)] = s_acts_ka_data[x]
+    for k in x.keys():
+        acts_ka_data_encoded[custom_encode(str(k))] = x[k]
 
 # with open(acts.json, r) as acts_ka_file:
 #     acts_ka_data = json.loads(acts_ka_file.read())
@@ -41,11 +44,11 @@ for x in s_acts_ka_data:
 #     cases_ka_data = json.loads(cases_ka_file.read())
 
 
-mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
-mongo_db = mongo_client["LKG_extended_data_base"]
-mongo_col1 = mongo_db["acts_ka_db"]
-mongo_col2 = mongo_db["cases_ka_db"]
+legal_client = pymongo.MongoClient("mongodb://localhost:27017/")
+legal_db = legal_client["LKG_extended_data_base"]
+acts_collection = legal_db["acts_ka_db"]
+cases_collection = legal_db["cases_ka_db"]
 
-x = mongo_col1.insert_one(acts_ka_data_encoded)
+x = acts_collection.insert_one(acts_ka_data_encoded)
 
 # y = mycol.insert_one(cases_ka_data)
