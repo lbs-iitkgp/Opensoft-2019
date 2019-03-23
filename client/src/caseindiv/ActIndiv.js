db257c953d6dbfd2e7663b9f9a969e73f252ee8f
rka87338 <rka87338@gmail.com>
2019-03-23 22:29:01 +0530

    soft coded act inidv-sec description

--- client/src/caseindiv/ActIndiv.js
+++ client/src/caseindiv/ActIndiv.js
@@ -15,45 +15,78 @@ const styles = theme => ({
       fontWeight: theme.typography.fontWeightRegular,
     },
   });
+
+  function createSections(section,index){
+    var secTitle = section[0];
+    var secDes = section[1]
+    return{index,secTitle,secDes}
+  }
+
+  var sectionsData =[
+    ['section-1','this is description of sec-1'],
+    ['section-2','this is description of sec-2'],
+    ['section-3','this is description of sec-13']
+  ].map((ele,ind)=>createSections(ele,ind));
   
 
   function ActIndiv(props) {
     const { classes } = props;
-    return (
-      <div className={classes.root}>
-        <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}><h3>sectionx-1</h3></Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              Act one descriotion   
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}><h3>sectionx-2</h3></Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-            Acts two decripton
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-        <ExpansionPanel>
-          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-            <Typography className={classes.heading}><h3>sectionx-3</h3></Typography>
-          </ExpansionPanelSummary>
-          <ExpansionPanelDetails>
-            <Typography>
-              Act one descriotion   
-            </Typography>
-          </ExpansionPanelDetails>
-        </ExpansionPanel>
-        <AdvFilter />
-       </div>
-    );
+
+      return(
+          <div className={classes.root}>
+          {sectionsData.map(ele => (
+            <ExpansionPanel>
+              <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography className={classes.heading}>
+                 <h3>{ele.secTitle}</h3>
+                </Typography>
+              </ExpansionPanelSummary>
+              <ExpansionPanelDetails>
+                <Typography>
+                  {ele.secDes}   
+                 </Typography>
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+
+          ))}
+            <AdvFilter />
+          </div>
+      );
+    // return (
+    //   <div className={classes.root}>
+    //     <ExpansionPanel>
+    //       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+    //         <Typography className={classes.heading}><h3>sectionx-1</h3></Typography>
+    //       </ExpansionPanelSummary>
+    //       <ExpansionPanelDetails>
+    //         <Typography>
+    //           Act one descriotion   
+    //         </Typography>
+    //       </ExpansionPanelDetails>
+    //     </ExpansionPanel>
+    //     <ExpansionPanel>
+    //       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+    //         <Typography className={classes.heading}><h3>sectionx-2</h3></Typography>
+    //       </ExpansionPanelSummary>
+    //       <ExpansionPanelDetails>
+    //         <Typography>
+    //         Acts two decripton
+    //         </Typography>
+    //       </ExpansionPanelDetails>
+    //     </ExpansionPanel>
+    //     <ExpansionPanel>
+    //       <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+    //         <Typography className={classes.heading}><h3>sectionx-3</h3></Typography>
+    //       </ExpansionPanelSummary>
+    //       <ExpansionPanelDetails>
+    //         <Typography>
+    //           Act one descriotion   
+    //         </Typography>
+    //       </ExpansionPanelDetails>
+    //     </ExpansionPanel>
+    //     <AdvFilter />
+    //    </div>
+    // );
   }
   
   
--- client/src/caseindiv/router.js
+++ client/src/caseindiv/router.js
@@ -20,7 +20,7 @@ class Router extends Component {
               <Route exact path='/' component={App} />  
               <Route exact path='/basic' component={BasicSearch} />
               <Route exact path="/judge" component={JudgeIndiv}  />
-              <Route exact path="/act/:id/:num" component={ActIndiv}  />
+              <Route exact path="/act" component={ActIndiv}  />
               <Route  exact path='/output' component={Output} />
               <Route exact path='/modal' component={Modal}  />
             </Switch>
