fce13c1221da38f6d17555017b4c39a538eb2e71
Shankusu7 <pankaj08072000@gmail.com>
2019-03-23 22:29:54 +0530

    renamed the encode and decode functions

--- api/encode_helper.py
+++ api/encode_helper.py
@@ -1,11 +1,11 @@
 import base64
 
 
-def enkode(s):
+def custom_encode(s):
     return base64.b64encode(s.encode()).decode()
 
 
-def dekode(s):
+def custom_decode(s):
     return base64.b64decode(s).decode()
 
 if __name__ == "__main__":
