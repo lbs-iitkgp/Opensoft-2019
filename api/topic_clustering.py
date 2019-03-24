c0401fe925db057243db1815fb87a9ba7c7700af
aribalam <aribalam64@gmail.com>
2019-03-24 17:45:47 +0530

    code modularization and bug fixes

--- api/topic_clustering.py
+++ api/topic_clustering.py
@@ -2,22 +2,6 @@ import os
 import spacy
 import json
 
-nlp = spacy.load('en_core_web_md')
-
-file = open(os.path.join(os.getcwd(), 'base_class', 'catch.json'), 'r')
-
-# Removes year from keywords
-catch_words = [item[0].lower() for item in json.loads(file.read()) if not item[0].isnumeric()]
-
-# make a line out of these words
-catch_words_line = " ".join(catch_words[:100])
-
-# remove duplicate words and join them
-catch_words_line = set(catch_words_line.split())
-catch_words_line = " ".join(catch_words_line)
-
-doc = nlp(catch_words_line)
-
 # for token in doc:
 #     if token.pos_ not in tags.keys():
 #         tags[token.pos_] = [token.text]
@@ -26,25 +10,51 @@ doc = nlp(catch_words_line)
 #
 # for key, val in tags.items():
 #     print(key, ': ', val)
+SIMILARITY_THRESHOLD = 0.6
 
-# form token only of nouns and adjectives
-tags = ['NOUN', 'ADJ']
-words = []
-for token in doc:
-    if token.pos_ in tags:
-        words.append(token)
 
-# make clusters with formed tokens
-clusters = []
-for word in words:
-    cluster = [word]
-    for word2 in words:
-        if word2.text == word.text:
-            continue
-        if word2.similarity(word) > 0.6:
-            cluster.append(word2.text)
-            words.remove(word2)
-    clusters.append(cluster)
-    words.remove(word)
+def get_topic_clusters(keywords, nlp):
 
-print(clusters)
+    # Removes year from keywords
+    keywords = [item.lower() for item in keywords if not item.isnumeric()]
+
+    # make a line out of these words
+    keywords_line = " ".join(keywords)
+    # for testing
+    # keywords_line = " ".join(keywords[:100])
+
+    # remove duplicate words and join them
+    keywords_line = set(keywords_line.split())
+    keywords_line = " ".join(keywords_line)
+
+    doc = nlp(keywords_line)
+
+    # form token only of nouns and adjectives
+    tags = ['NOUN', 'ADJ']
+    words = []
+    for token in doc:
+        if token.pos_ in tags:
+            words.append(token)
+
+    # make clusters with formed tokens
+    clusters = []
+    for word in words:
+        cluster = [word]
+        for word2 in words:
+            if word2.text == word.text:
+                continue
+            if word2.similarity(word) > SIMILARITY_THRESHOLD:
+                cluster.append(word2.text)
+                words.remove(word2)
+        clusters.append(cluster)
+        words.remove(word)
+
+    return clusters
+
+
+if __name__ == '__main__':
+    nlp = spacy.load('en_core_web_md')
+    print(os.getcwd())
+    file = open(os.path.join(os.getcwd(), 'api', 'base_class', 'catch.json'), 'r')
+    catch_words = [item[0] for item in json.loads(file.read())]
+    print(get_topic_clusters(catch_words, nlp))
