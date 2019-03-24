0da151f3dd6351a9650f4598b7f7c2b168517bd5
vinay612 <vinayagarwal612@gmail.com>
2019-03-24 06:55:45 +0000

    Final Codes fro neo4j conversion

--- api/base_class/graph_sample.py
+++
@@ -1,6 +0,0 @@
-import json
-import networkx as nx
-from graph_io import import_graph
-
-# graph=import_graph('Documents/Opensoft-2019/api/base_class/'+'samplelegalknowledgegraph.json')
-graph = import_graph('LegalKnowledgeGraph.json')
---
+++ api/base_class/neo4j_to_networkx_graph.py
@@ -0,0 +1,45 @@
+from neo4jrestclient.client import GraphDatabase
+from neo4jrestclient import client
+import networkx as nx
+import matplotlib.pyplot as plt
+from env import ENV
+
+'''
+    This function reads the NEO4J graph stored in the neo4j browser and converts it into a networkx graph
+'''
+def export_neo4j():
+    types=['judge','keyword','case','catch']
+    graph = nx.DiGraph()
+    db = GraphDatabase(ENV["DB_URL"], username=ENV["DB_USERNAME"], password=ENV["DB_PASSWORD"]) # Authentication for NEO4J Browser
+    for i in range(len(types)):
+        print(types[i])
+        q = 'MATCH (c:'+types[i]+') return c'   #Quering for all nodes in the graph
+        results = db.query(q)
+        for record in results:
+            props={}
+            node = record[0]
+            if node:
+                label=node['metadata']['labels']
+                id=node['data']['id']
+                node['data'].pop('id',None)
+                props= node['data']
+                props["type"] = label
+                graph.add_node(id, **props)
+            #  print(props)
+    for i in range(len(types)):
+        for j in range(len(types)):
+            q='MATCH (c:'+types[i]+')-[r]->(m:'+types[j]+') return c,m' # Quering for all Relationships in the graph
+            results=db.query(q)
+            for record in results:
+                node1,node2=record
+                graph.add_edge(node1['data']['id'],node2['data']['id'])
+
+    print('Number of edges in the networkx graph = ',graph.number_of_edges())
+    print('Number of nodes in the networkx graph = ',graph.number_of_nodes())
+#    nx.draw(graph,with_labels=True)
+#    plt.draw()
+#    plt.show()
+
+
+if __name__ == "__main__":
+    export_neo4j()
