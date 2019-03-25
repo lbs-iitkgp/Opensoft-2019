9efe3c786cc8cc1d2ceb3bd58ff02b55e82e935c
aribalam <aribalam64@gmail.com>
2019-03-26 00:39:14 +0530

    created function for sentence clustering

--- api/topic_clustering.py
+++ api/topic_clustering.py
@@ -112,6 +112,42 @@ def get_topic_clusters_with_count(keywords, nlp):
     return clusters
 
 
+def process(text):
+    return " ".join([word for word in text.split() if not word.isnumeric()])
+
+
+def get_sentence_clusters(keywords, nlp):
+    # make a line out of these words
+    keywords = [item for item in keywords if not item.isnumeric()]
+
+    # keywords_line = " ".join(keywords)
+    # for testing
+    # keywords_line = " ".join(keywords[:100])
+
+    # remove duplicate words and join them
+    keywords_line = keywords
+
+    words_used = []
+    # make clusters with formed tokens
+    clusters = []
+    for text in keywords_line:
+        if text in words_used:
+            continue
+        doc1 = nlp(process(text))
+        cluster = [text]
+        for text2 in keywords_line:
+            if text2 == text or text2 in words_used:
+                continue
+            doc2 = nlp(process(text2))
+            if doc1.similarity(doc2) > 0.7:
+                cluster.append(text2)
+                words_used.append(text2)
+        clusters.append(cluster)
+        words_used.append(text)
+
+    return clusters
+
+
 def is_similar(word1, word2, clusters):
     """
     Checks if two strings belong to the cluster
@@ -132,24 +168,28 @@ if __name__ == '__main__':
 
     nlp = spacy.load('en_core_web_md')
 
-    file = open(os.path.join(os.getcwd(), 'api', 'base_class', 'keyword.json'), 'r')
+    file = open(os.path.join(os.getcwd(), 'api', 'base_class', 'catch.json'), 'r')
 
-    # catch_words = [item[0] for item in json.loads(file.read())]
-    # # Removes year from keywords
+    catch_words = [item[0].lower() for item in json.loads(file.read())]
+    # catch_words.append("criminal")
+    # catch_words.append("crime")
+    # catch_words.append("killing")
+
+    # Removes year from keywords
     # catch_words = [item.lower() for item in catch_words if not item.isnumeric()]
+
+    clusters = get_sentence_clusters(catch_words, nlp)
+
+    # catch_words = {item[0].lower(): item[1] for item in json.loads(file.read()) if not item[0].isnumeric()}
     #
-    # clusters = get_topic_clusters(catch_words, nlp)
+    # clusters = get_topic_clusters_with_count(catch_words, nlp)
 
-    catch_words = {item[0].lower(): item[1] for item in json.loads(file.read()) if not item[0].isnumeric()}
+    print(len(clusters))
 
-    clusters = get_topic_clusters_with_count(catch_words, nlp)
-
-    print(clusters)
-
-    word1 = input("Enter first word : ")
-    word2 = input("Enter second word : ")
-
-    print(is_similar(word1, word2, clusters))
+    # word1 = input("Enter first word : ")
+    # word2 = input("Enter second word : ")
+    #
+    # print(is_similar(word1, word2, clusters))
 
 
 
