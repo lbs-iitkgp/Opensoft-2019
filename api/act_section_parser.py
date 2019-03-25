8cd19433ba7d4b530cdd44aa6d8a6d9625c16897
thebhatman <manjunathbhat9920@gmail.com>
2019-03-25 09:53:03 +0530

    Relatively better extraction this time

--- api/act_section_parser.py
+++ api/act_section_parser.py
@@ -4,6 +4,7 @@ import os
 import json
 import editdistance
 import difflib
+import regex as re
 
 all_files = os.listdir("./All_FT")
 all_cases = filter(lambda x: x[-4:] == ".txt", all_files)
@@ -36,8 +37,9 @@ def fetch_acts_from_cases(all_acts):
 	case_dict = {}
 	act_cases = {}
 	total_num_cases = len(all_cases)
-	for j in range(total_num_cases):
+	for j in range(100):
 		file = open("./All_FT/" + all_cases[j], 'r')
+		#file = open("./All_FT/2003_C_16.txt", 'r')
 		text = ""
 		line_num = 1
 		#print(all_cases[j])
@@ -45,6 +47,8 @@ def fetch_acts_from_cases(all_acts):
 		act_cases[all_cases[j]] = set()
 		prev_act_name = ""
 		year = ""
+		dada_act_name = ""
+		acts_so_far = {}
 		for line in file:
 			if "u/s." in line :
 				line = line.replace("u/s.", "Section")
@@ -60,84 +64,163 @@ def fetch_acts_from_cases(all_acts):
 				line = line.replace("this Act", prev_act_name)
 			if "that Act" in line:
 				line = line.replace("that Act", prev_act_name)
+			if "subsection" in line:
+				line = line.replace("subsection", "Section")
+			if "subSection" in line:
+				line = line.replace("subSection", "Section")
+			if "the Act" in line:
+				print("*********", line_num )
+				print("&&&&&&&&&&",prev_act_name)
+				line = line.replace("the Act", prev_act_name)
+			if "this Act" in line:
+				print("*********", line_num)
+				print("&&&&&&&&&&",prev_act_name)
+				line = line.replace("this Act", prev_act_name)
+			if "that Act" in line:
+				print("*********", line_num )
+				print("&&&&&&&&&&",prev_act_name)
+				line = line.replace("that Act", prev_act_name)
+			if "the said Act" in line:
+				print("*********", line_num)
+				print("&&&&&&&&", prev_act_name)
+			actual_line = line
+			line = re.sub(r"[-()\"#/'@;<>{}`+=~|!?]", '', line)
 			text += line
 			doc = nlp(text)
 			line_words = line.split(" ")
+			actual_line_words = actual_line.split(" ")
 			for entity in doc.ents:
 				act_bool = 0
 				words = entity.text.split(" ")
-				if entity.label_ == u"LAW" :
+				#if entity.label_ == u"LAW" or entity.label_ == u'ORG':
 					#print("Spacey : ", entity.text)
-					if "Act" in words or "Act," in words or "Act." in words:
-						act_bool = 1
-						if words[0] in BAD_WORDS_TYPE1:
-							words = words[1:]
+				if "Act" in words or "Act," in words or "Act." in words:
+					act_bool = 1
+					if words[0] in BAD_WORDS_TYPE1:
+						words = words[1:]
+					year = ""
+					ent_ind = line.index(entity.text)
+					ent_ind = ent_ind + len(entity.text)
+					while ent_ind < len(line):
+						if line[ent_ind] >= '0' and line[ent_ind] <= '9':
+							year += line[ent_ind]
+						ent_ind += 1
+					if len(year) != 4:
+						year = ""
 
-						act_name = ""
-						for word in words:
-							if act_name == "":
-								act_name = act_name + word
-							else:
-								act_name = act_name + " " + word 
-						
-						parts = act_name.split(" ")
-						
-						sect_flag = 0
-						sect_num = -1
-						if parts[0] in BAD_WORDS_TYPE2:
-							i = 0
-							sect_flag = 1
-							a = 0
-							parts_len = len(parts)
-							while a < parts_len:
-								if parts[a][0] >= '0' and parts[a][0] <= '9':
-									sect_num = parts[a]
-									break
-								a = a + 1
-							while parts[i] not in BAD_WORDS_TYPE1:
-								i = i + 1
-								if i >= parts_len:
-									break
-							parts = parts[i + 1:]
-							act_name = ""
-							for part in parts:
-								if act_name == "":
-									act_name = act_name + part
-								else:
-									act_name = act_name +" " + part
-
-						if len(parts) <= 1:
-							continue
-
-						prev_act_name = act_name
-						#print("Yo, ", prev_act_name)
-						if prev_act_name == "":
-							continue
-						if len(difflib.get_close_matches(prev_act_name, all_acts)) > 0:
-							prev_act_name = difflib.get_close_matches(prev_act_name, all_acts)[0]
-						#else:
-						#	print(prev_act_name)
-
-						ind = 0
-						sect_num = ""
-						line_length = len(line)
-						if "Section" in line:
-							ind = line.index("Section")
-							ind = ind + 7
-						while(line[ind] == " "):
-							ind = ind + 1
-							if ind >= line_length:
+					act_name = ""
+					for word in words:
+						if act_name == "":
+							act_name = act_name + word
+						else:
+							act_name = act_name + " " + word 
+					
+					parts = act_name.split(" ")
+					
+					sect_flag = 0
+					sect_num = -1
+					if parts[0] in BAD_WORDS_TYPE2:
+						i = 0
+						sect_flag = 1
+						a = 0
+						parts_len = len(parts)
+						while a < parts_len:
+							if parts[a][0] >= '0' and parts[a][0] <= '9':
+								sect_num = parts[a]
 								break
-						if ind < line_length:
-							while(line[ind] != " "):
-								sect_num += line[ind]
-								ind += 1
-								if ind >= line_length:
-									break
-						if prev_act_name not in case_dict[all_cases[j]]:
-							act_cases[all_cases[j]].add(prev_act_name)
-						if sect_flag and prev_act_name != "":
-							act_cases[all_cases[j]].add(prev_act_name)
+							a = a + 1
+						while parts[i] not in BAD_WORDS_TYPE1:
+							i = i + 1
+							if i >= parts_len:
+								break
+						parts = parts[i + 1:]
+						act_name = ""
+						for part in parts:
+							if act_name == "":
+								act_name = act_name + part
+							else:
+								act_name = act_name +" " + part
+
+					if len(parts) <= 1:
+						continue
+
+					dada_act_name = prev_act_name
+					prev_act_name = act_name
+					if year != "":
+						prev_act_name = prev_act_name + " " + year
+					parsed_by_spacy = prev_act_name
+
+					print("Yo, ", prev_act_name)
+					if prev_act_name == "":
+						continue
+					if parsed_by_spacy in acts_so_far:
+						prev_act_name = acts_so_far[parsed_by_spacy]
+					elif len(difflib.get_close_matches(prev_act_name, all_acts, 5)) > 0:
+						possible = difflib.get_close_matches(prev_act_name, all_acts, 5)
+						max_count = 0
+						#print(possible)
+						for i in range(len(possible)):
+							match_count = 0
+							bas = possible[i]
+							w = possible[i].split(" ")
+							for a in w:
+								if a.lower() in [f.lower() for f in line_words]:
+									match_count += 1
+							if match_count >= max_count:
+								max_count = match_count
+								prev_act_name = possible[i]
+						checker = prev_act_name.split(" ")
+						if checker[0].lower() not in [v.lower() for v in line_words]:
+							if dada_act_name != "":
+								prev_act_name = dada_act_name
+					print(prev_act_name)
+					if parsed_by_spacy not in acts_so_far:
+						acts_so_far[parsed_by_spacy] = prev_act_name
+					line_of_act = 0
+					loc_of_act = 0
+					line_of_act = line_num
+					for each_word in prev_act_name.split(" "):
+						if each_word in line_words:
+							loc_of_act = line_words.index(each_word)
+
+					# if len(difflib.get_close_matches(prev_act_name, all_acts, 5)) > 0:
+					# 	possible = difflib.get_close_matches(prev_act_name, all_acts, 5)
+					# 	max_count = 0
+					# 	#print(possible)
+					# 	for i in range(len(possible)):
+					# 		match_count = 0
+					# 		bas = possible[i]
+					# 		w = possible[i].split(" ")
+					# 		for a in w:
+					# 			if a.lower() in [f.lower() for f in line_words]:
+					# 				match_count += 1
+					# 		if match_count >= max_count:
+					# 			max_count = match_count
+					# 			prev_act_name = possible[i]
+					#print(prev_act_name)
+					
+					# ind = 0
+					# sect_num = ""
+					# line_length = len(line)
+					# if "Section" in line:
+					# 	ind = line.index("Section")
+					# 	ind = ind + 7
+					# while(line[ind] == " "):
+					# 	ind = ind + 1
+					# 	if ind >= line_length:
+					# 		break
+					# if ind < line_length:
+					# 	while(line[ind] != " "):
+					# 		sect_num += line[ind]
+					# 		ind += 1
+					# 		if ind >= line_length:
+					# 			break
+					# if prev_act_name not in case_dict[all_cases[j]]:
+					# 	act_cases[all_cases[j]].add(prev_act_name)
+					# if sect_flag and prev_act_name != "":
+					# 	act_cases[all_cases[j]].add(prev_act_name)
+					if prev_act_name != "":
+						act_cases[all_cases[j]].add(prev_act_name)
 								
 			text = ""
 	for case in act_cases:
@@ -152,14 +235,18 @@ def fetch_section_act_mapping_from_case(all_acts):
 
 	case_dict = {}
 	total_num_cases = len(all_cases)
-	for j in range(total_num_cases):
+	for j in range(100):
+		#all_cases[j] = "2003_C_16.txt"
 		file = open("./All_FT/" + all_cases[j], 'r')
+		#file = open("./All_FT/2003_C_16.txt", 'r')
 		text = ""
-		line_num = 1
+		line_num = 0
 		print(all_cases[j])
 		case_dict[all_cases[j]] = {}
 		prev_act_name = ""
+		dada_act_name = ""
 		year = ""
+		acts_so_far = {}
 		for line in file:
 			if "u/s." in line :
 				line = line.replace("u/s.", "Section")
@@ -169,104 +256,178 @@ def fetch_section_act_mapping_from_case(all_acts):
 				line = line.replace("S.", "Section")
 			if "section" in line:
 				line = line.replace("section", "Section")
+			if "subsection" in line:
+				line = line.replace("subsection", "Section")
+			if "subSection" in line:
+				line = line.replace("subSection", "Section")
 			if "the Act" in line:
+				print("*********", line_num )
+				print("&&&&&&&&&&",prev_act_name)
 				line = line.replace("the Act", prev_act_name)
 			if "this Act" in line:
+				print("*********", line_num)
+				print("&&&&&&&&&&",prev_act_name)
 				line = line.replace("this Act", prev_act_name)
 			if "that Act" in line:
+				print("*********", line_num )
+				print("&&&&&&&&&&",prev_act_name)
 				line = line.replace("that Act", prev_act_name)
+			if "the said Act" in line:
+				print("*********", line_num)
+				print("&&&&&&&&", prev_act_name)
+			actual_line = line
+			line = re.sub(r"[-()\"#/'@;<>{}`+=~|!?]", '', line)
 			text += line
 			doc = nlp(text)
+			line_num += 1
 			line_words = line.split(" ")
+			actual_line_words = actual_line.split(" ")
 			for entity in doc.ents:
 				act_bool = 0
 				words = entity.text.split(" ")
-				if entity.label_ == u"LAW" :
+				# if entity.label_ == u"LAW" or entity.label_ == u'ORG':
 					#print("Spacey : ", entity.text)
-					if "Act" in words or "Act," in words or "Act." in words:
-						act_bool = 1
-						if words[0] in BAD_WORDS_TYPE1:
-							words = words[1:]
+				if "Act" in words or "Act," in words or "Act." in words:
+					act_bool = 1
+					if words[0] in BAD_WORDS_TYPE1:
+						words = words[1:]
+					year = ""
+					ent_ind = line.index(entity.text)
+					ent_ind = ent_ind + len(entity.text)
+					while ent_ind < len(line):
+						if line[ent_ind] >= '0' and line[ent_ind] <= '9':
+							year += line[ent_ind]
+						ent_ind += 1
+					if len(year) != 4:
+						year = ""
 
-						act_name = ""
-						for word in words:
-							if act_name == "":
-								act_name = act_name + word
-							else:
-								act_name = act_name + " " + word 
-						
-						parts = act_name.split(" ")
-						
-						sect_flag = 0
-						sect_num = -1
-						if parts[0] in BAD_WORDS_TYPE2:
-							i = 0
-							sect_flag = 1
-							a = 0
-							parts_len = len(parts)
-							while a < parts_len:
-								if parts[a][0] >= '0' and parts[a][0] <= '9':
-									sect_num = parts[a]
-									break
-								a = a + 1
-							while parts[i] not in BAD_WORDS_TYPE1:
-								i = i + 1
-								if i >= parts_len:
-									break
-							parts = parts[i + 1:]
-							act_name = ""
-							for part in parts:
-								if act_name == "":
-									act_name = act_name + part
-								else:
-									act_name = act_name +" " + part
-
-						if len(parts) <= 1:
-							continue
-
-						prev_act_name = act_name
-						#print("Yo, ", prev_act_name)
-						if prev_act_name == "":
-							continue
-						if len(difflib.get_close_matches(prev_act_name, all_acts)) > 0:
-							prev_act_name = difflib.get_close_matches(prev_act_name, all_acts)[0]
-						#else:
-						#	print(prev_act_name)
-						print(prev_act_name)
-						ind = 0
-						sect_num = ""
-						line_length = len(line)
-						if "Section" in line:
-							ind = line.index("Section")
-							ind = ind + 7
-						while(line[ind] == " "):
-							ind = ind + 1
-							if ind >= line_length:
+					act_name = ""
+					for word in words:
+						if act_name == "":
+							act_name = act_name + word
+						else:
+							act_name = act_name + " " + word 
+					
+					parts = act_name.split(" ")
+					
+					sect_flag = 0
+					sect_num = -1
+					if parts[0] in BAD_WORDS_TYPE2:
+						i = 0
+						sect_flag = 1
+						a = 0
+						parts_len = len(parts)
+						while a < parts_len:
+							if parts[a][0] >= '0' and parts[a][0] <= '9':
+								sect_num = parts[a]
 								break
-						if ind < line_length:
-							while(line[ind] != " "):
-								sect_num += line[ind]
-								ind += 1
-								if ind >= line_length:
-									break
-						if prev_act_name not in case_dict[all_cases[j]]:
-							case_dict[all_cases[j]][prev_act_name] = set()
+							a = a + 1
+						while parts[i] not in BAD_WORDS_TYPE1:
+							i = i + 1
+							if i >= parts_len:
+								break
+						parts = parts[i + 1:]
+						act_name = ""
+						for part in parts:
+							if act_name == "":
+								act_name = act_name + part
+							else:
+								act_name = act_name +" " + part
 
-						if sect_flag and prev_act_name != "":
-							case_dict[all_cases[j]][prev_act_name].add(sect_num)
+					if len(parts) <= 1:
+						continue
+					
+					dada_act_name = prev_act_name
+					prev_act_name = act_name
+					if year != "":
+						prev_act_name = prev_act_name + " " + year
+					print("Yo, ", prev_act_name)
+					if prev_act_name == "":
+						continue
+
+					# if len(difflib.get_close_matches(prev_act_name, all_acts)) > 0:
+					# 	prev_act_name = difflib.get_close_matches(prev_act_name, all_acts)[0]
+					#else:
+					#	print(prev_act_name)
+					parsed_by_spacy = prev_act_name
+					#if "referred" in line_words and ("Act" in line_words or "Act," in line_words or "Act." in line_words):
+
+					#parsed_by_spacy = re.sub('[^A-Za-z0-9]+', '', parsed_by_spacy)
+					if parsed_by_spacy in acts_so_far:
+						prev_act_name = acts_so_far[parsed_by_spacy]
+					elif len(difflib.get_close_matches(prev_act_name, all_acts, 5)) > 0:
+						possible = difflib.get_close_matches(prev_act_name, all_acts, 5)
+						max_count = 0
+						#print(possible)
+						for i in range(len(possible)):
+							match_count = 0
+							bas = possible[i]
+							w = possible[i].split(" ")
+							for a in w:
+								if a.lower() in [f.lower() for f in line_words]:
+									match_count += 1
+							if match_count >= max_count:
+								max_count = match_count
+								prev_act_name = possible[i]
+						checker = prev_act_name.split(" ")
+						if checker[0].lower() not in [v.lower() for v in line_words]:
+							if dada_act_name != "":
+								prev_act_name = dada_act_name
+					print(prev_act_name)
+					if parsed_by_spacy not in acts_so_far:
+						acts_so_far[parsed_by_spacy] = prev_act_name
+					line_of_act = 0
+					loc_of_act = 0
+					line_of_act = line_num
+					for each_word in prev_act_name.split(" "):
+						if each_word in line_words:
+							loc_of_act = line_words.index(each_word)
+
+					ind = 0
+					sect_num = set()
+					while ind < len(actual_line_words):
+						if actual_line_words[ind] == "Section":
+							sect_num.add(actual_line_words[ind + 1])
+						ind = ind + 1
+					if prev_act_name not in case_dict[all_cases[j]] and prev_act_name != "":
+						case_dict[all_cases[j]][prev_act_name] = {}
+						case_dict[all_cases[j]][prev_act_name]["Section"] = sect_num
+						case_dict[all_cases[j]][prev_act_name]["Location"] = (line_num, loc_of_act)
+
+
+					# line_length = len(line)
+					# if "Section" in line:
+					# 	ind = line.index("Section")
+					# 	ind = ind + 7
+					# while(line[ind] == " "):
+					# 	ind = ind + 1
+					# 	if ind >= line_length:
+					# 		break
+					# if ind < line_length:
+					# 	while(line[ind] != " "):
+					# 		sect_num += line[ind]
+					# 		ind += 1
+					# 		if ind >= line_length:
+					# 			break
+					# if prev_act_name not in case_dict[all_cases[j]]:
+					# 	case_dict[all_cases[j]][prev_act_name] = {}
+					# 	case_dict[all_cases[j]][prev_act_name]["Section"] = set()
+					# 	case_dict[all_cases[j]][prev_act_name]["Location"] = (line_num, loc_of_act)
+					# if sect_flag and prev_act_name != "":
+					# 	case_dict[all_cases[j]][prev_act_name]["Section"].add(sect_num)
 						
-					elif "Section" in words:
-						t = words.index("Section") + 1
-						if t < len(words):
-							sect_num = words[t]
-							if prev_act_name != "":
-								case_dict[all_cases[j]][prev_act_name].add(sect_num)
+				if "Section" in words:
+					t = words.index("Section") + 1
+					if t < len(words):
+						sect_num = words[t]
+						if prev_act_name != "":
+							case_dict[all_cases[j]][prev_act_name]["Section"].add(sect_num)
 
 				
 			text = ""
 	for case_id in case_dict:
 		for act in case_dict[case_id]:
-			case_dict[case_id][act] = list(case_dict[case_id][act])
+			case_dict[case_id][act]["Section"] = list(case_dict[case_id][act]["Section"])
 	with open("section_act_mapping_from_case.json", "w") as write_file:
 		json.dump(case_dict, write_file, indent = 4)
 	return case_dict
