c8e868a98d1fe76bae1605b0bba491c245a66a05
Divyang Mittal <37113922+divyang-mittal@users.noreply.github.com>
2019-03-28 20:52:52 +0530

    Update case.py

--- api/endpoints/case.py
+++ api/endpoints/case.py
@@ -1,6 +1,7 @@
 from endpoints import *
 
 @app.route('/case/<case_id>', methods=['GET'])
+@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def case_metadata(case_id):
     case = mongo_db.find("case_id",case_id)
     # case  = {   'case_name': "hi",
@@ -22,6 +23,7 @@ def case_metadata(case_id):
     return jsonify(result)
 
 @app.route('/case/<case_id>/plot_line', methods=['GET'])
+@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def case_line_distribution(case_id):
     result = {}
     for i in range (1947 ,2020):
@@ -39,6 +41,7 @@ def case_line_distribution(case_id):
 
 
 @app.route('/case/<case_id>/timeline', methods=['GET'])
+@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def case_timeline(case_id):
     # Timeline function w/ sections parser
     #  # [
@@ -72,6 +75,7 @@ def case_timeline(case_id):
     return('Hello')
 
 @app.route('/case/<case_id>/citations', methods=['GET'])
+@cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def case_citations(case_id):
     # Get citer id's from neo4j, and respective names from mongo
     #
