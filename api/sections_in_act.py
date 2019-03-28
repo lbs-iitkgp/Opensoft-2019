d23d36f4eec99f7033935e0ed3360a318ddb2d89
thebhatman <manjunathbhat9920@gmail.com>
2019-03-29 01:56:05 +0530

    Script for obtaining text of a section of an act

---
+++ api/sections_in_act.py
@@ -0,0 +1,37 @@
+from __future__ import unicode_literals
+import os
+import json
+all_central_acts = os.listdir("./Acts/Central_Text")
+ 
+# This function returns all the sections in a given act. The input has 
+# to be the path of the act file.
+def get_sections_in_act(act_path):
+	file = open(act_path)
+	all_sections = []
+	for line in file:
+		sect_num = ""
+		if "Section" in line:
+			ind = line.index("Section")
+			ind = ind + 8
+			while(line[ind] != "-"):
+				sect_num += line[ind]
+				ind += 1;
+				if ind > len(line):
+					break
+			all_sections.append(sect_num)
+	return all_sections
+# This functions returns the text in the section of a given act.
+# The input has to be path of the act file and section number.
+def get_text_in_section(act_path, section_num):
+	sect_num = "Section "
+	sect_num += str(section_num)
+	file = open(act_path)
+	for line in file:
+		if sect_num in line:
+			return line
+# Sample usage of the functions.
+# sect_list = get_sections_in_act("/home/manjunath/Downloads/OpenSoft-Data/Acts/State_Text/gujrat/8_1.txt")
+# line = get_text_in_section("/home/manjunath/Downloads/OpenSoft-Data/Acts/State_Text/gujrat/8_1.txt",10)
+# print(sect_list)
+# print("*********")
+# print(line)
