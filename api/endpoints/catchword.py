fdb5766615e72bedf615a4f2245e59e0b0b0ef3c
aribalam <aribalam64@gmail.com>
2019-03-29 01:26:24 +0530

    update catchword.py

--- api/endpoints/catchword.py
+++ api/endpoints/catchword.py
@@ -1,21 +1,20 @@
 from endpoints import *
 
+
 @app.route('/catchword/<catchword_id>', methods=['GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def catchword_metadata(catchword_id):
     # Just return catchword name, # of cases and precentile among catchwords maybe?
-    catchword = mydb.mytable.find({"catchword_id":catchword_id})
-    result ={
-        'name': catchword["name"],
-        'number_of_cases': catchword["number_of_cases"],
-        'percentile': mydb.mytable.find({"catchword_id":catchword_id}).count()*100.0/mydb.mytable.count()
-    }
-    return jsonify(result)
+    catch_word = mgdb_handler.read_all(catch_collection, serial=catchword_id)[0]
+    number_of_cases = len(get_metas_from_node(catchword_id, "catch", "case"))
+    catch_word["number_of_cases"] = number_of_cases
+
+    return jsonify(catch_word)
 
 
 @app.route('/catchword/<catchword_id>/plot_line', methods=['GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
-def catchword_cases(catchword_id):
+def catchword_line_distribution(catchword_id):
     # Iterate through each citer in neo4j
     #   Find citer's year from mongo
     # nx_graph = export_neo4j()
@@ -36,28 +35,17 @@ def catchword_cases(catchword_id):
         result[int(year)] += 1
     return jsonify(result)
 
+
 @app.route('/catchword/<catchword_id>/cases', methods=['GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def catchword_cases(catchword_id):
-# # Fetch list of cases that cite this catchword from neo4j and return their details from mongodb as json
-#     # nx_graph = export_neo4j()
     result = []
-    for i in range (1947,2020):
-        result[i] = 0
-    subgraph = lkg.query(judges =[], subjects=[catchword_id], keywords=[], judgements = [], types =[], year_range=[], acts = [])
-    
-    for node in subgraph['nodes']:
-        case = mongo_db.find("case_id",node['case_id'])
-        point = {
-            "case_id": case['case_id'],
-            "case_name": case['case_name'],
-            "case_indlaw_id": case['case_indlawid'],
-            "case_judges": case['judge'],
-            "case_judgement": case['judgement'],
-            "case_date": case['case_date'],
-            "case_year": case['case_year']
-            }
-        result.append(point)
+
+    case_ids = get_metas_from_node(catchword_id, "catch", "case")
+    for id in case_ids:
+        case = get_metas_from_node(catchword_id, "catch", "case")
+        result.append(case)
+
     return jsonify(result)
 
 
