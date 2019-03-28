16023fae2f9c24a90d0c2abfc5cbd444d8f8594e
aribalam <aribalam64@gmail.com>
2019-03-28 23:58:11 +0530

    updated all judge endpoints

---
+++ api/endpoints/judge_new.py
@@ -0,0 +1,47 @@
+from endpoints import *
+
+
+# TODO: from graph
+@app.route('/judge/<judge_id>', methods=['GET'])
+def judge_metadata(judge_id):
+    judge = mgdb_handler.read_all(judges_collection, serial=judge_id)[0]
+
+    cases_count = len(get_metas_from_node(judge_id, "judge", "case"))
+    judge["number_of_cases"] = cases_count
+
+    return jsonify(judge)
+
+
+# TODO: From graph
+@app.route('/judge/<judge_id>/plot_line', methods=['GET'])
+def judge_line_distribution(judge_id):
+    # Iterate through each case of judge in neo4j
+    #   Find case's year from mongo
+    # nx_graph = export_neo4j()
+    result = []
+    for i in range(1947, 2020):
+        result[i] = 0
+    subgraph = lkg.query(judges=[judge_id], subjects=[], keywords=[], judgements=[], types=[], year_range=[])
+
+    data = lkg.nodes(data=True)
+    such_cases = subgraph[judge_id]
+    for case in such_cases:
+        all_metas = lkg.in_edges(case)
+        for meta, _ in all_metas:
+            if data[meta]['type'] == 'year':
+                year = meta
+        result[int(year)] += 1
+    return jsonify(result)
+
+
+# TODO: From graph
+@app.route('/judge/<judge_id>/cases', methods=['GET'])
+def judge_cases(judge_id):
+    result = []
+
+    case_ids = get_metas_from_node(judge_id, "judge", "case")
+    for id in case_ids:
+        case = get_metas_from_node(judge_id, "judge", "case")
+        result.append(case)
+
+    return jsonify(result)
