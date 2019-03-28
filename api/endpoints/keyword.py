1a2df9b0994a833509e44581427cd0ead28427c9
aribalam <aribalam64@gmail.com>
2019-03-29 01:32:15 +0530

    update keyword.py

--- api/endpoints/keyword.py
+++ api/endpoints/keyword.py
@@ -4,14 +4,12 @@ from endpoints import *
 @app.route('/keyword/<keyword_id>', methods=['GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def keyword_metadata(keyword_id):
-    keyword = mongo_db.find("keyword_id", keyword_id)  
-    # keyword ={'name': "Hot Damn",'number_of_cases':10}    
-    result = {
-      "name": keyword['name'],
-      "number_of_cases": keyword['number_of_cases'],
-      "percentile": mongo_db.find({"keyword_id":keyword_id}).count()*100.0/mongo_db.count() 
-    }
-    return jsonify(result)
+    key_word = mgdb_handler.read_all(keyword_collection, serial=keyword_id)[0]
+    number_of_cases = len(get_metas_from_node(keyword_id, "keyword", "case"))
+    key_word["number_of_cases"] = number_of_cases
+
+    return jsonify(key_word)
+
 
 @app.route('/keyword/<keyword_id>/plot_line', methods=['GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
@@ -34,26 +32,15 @@ def keyword_line_distribution(keyword_id):
         result[int(year)] += 1
     return jsonify(result)
 
+
 @app.route('/keyword/<keyword_id>/cases', methods=['GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def keyword_cases(keyword_id):
-#     # Fetch list of cases that cite this keyword from neo4j and return their details from mongodb as json
-#     nx_graph = export_neo4j()
     result = []
-    for i in range (1947,2020):
-        result[i] = 0
-    subgraph = lkg.query(judges =[], subjects=[], keywords=[keyword_id], judgements = [], types =[], year_range=[],acts = [])
-    
-    for node in subgraph['nodes']:
-        case = mongo_db.find("case_id",node['case_id'])
-        point = {
-            "case_id": case['case_id'],
-            "case_name": case['case_name'],
-            "case_indlaw_id": case['case_indlawid'],
-            "case_judges": case['judge'],
-            "case_judgement": case['judgement'],
-            "case_date": case['case_date'],
-            "case_year": case['case_year']
-            }
-        result.append(point)
+
+    case_ids = get_metas_from_node(keyword_id, "keyword", "case")
+    for id in case_ids:
+        case = get_metas_from_node(keyword_id, "keyword", "case")
+        result.append(case)
+
     return jsonify(result)
