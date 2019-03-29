b20017378bceb81b39e87b49bb889aead3d37d18
Athitya <athityakumar@gmail.com>
2019-03-29 19:20:23 +0530

    fix es

--- api/endpoints/search.py
+++ api/endpoints/search.py
@@ -32,10 +32,10 @@ def fetch_cards():
         judge = get_doc_with_maxscore(query, 'judge')
         act = get_doc_with_maxscore(query, 'act')
         cards = {
-            'judges': set(judge),
-            'year_range' : set(year_range),
-            'subjects' : set(subjects),
-            'acts' : set(act)
+            'judges': list(set(judge)),
+            'year_range' : list(set(year_range)),
+            'subjects' : list(set(subjects)),
+            'acts' : list(set(act))
         }
         return jsonify(cards)
         # case = get_doc_with_maxscore(query, 'case')
