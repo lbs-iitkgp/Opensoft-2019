55d630736ad4d74f90e27e72978691285da4863a
Athitya <athityakumar@gmail.com>
2019-03-29 17:04:57 +0530

    Changes import from json to neo4j

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -12,7 +12,8 @@ app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 cors = CORS(app, resources={r"/": {"origins": "http://localhost:3000"}})
-lkg = json_to_lkg("LKG.json")
+# lkg = json_to_lkg("LKG.json")
+lkg = neo4j_to_lkg()
 
 acts_collection = "act_db"
 recent_acts_collection = "recent_act_db"
