5c36a30e95190c982acd438020bc7a9b3767db90
Athitya <athityakumar@gmail.com>
2019-03-29 17:53:58 +0530

    Fixes endpoint error

--- api/endpoints/__init__.py
+++ api/endpoints/__init__.py
@@ -2,7 +2,6 @@ from flask import request, Flask, jsonify
 from flask_cors import CORS, cross_origin
 from backend.graph_io import json_to_lkg
 from sections_in_act import get_sections_in_act, get_text_in_section
-from endpoints.common import *
 from nlp.timeline import get_timelines
 from backend.graph_io import *
 
@@ -26,3 +25,5 @@ keyword_collection = "keyword_db"
 years_collection = "year_db"
 
 nlp = spacy.load('en')
+
+from endpoints.common import *
