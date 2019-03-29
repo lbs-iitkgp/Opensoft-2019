9abe21c53af5edbb4cb832d536802f431f2cad66
Pankaj kumar agarwal <43635589+Shankusu993@users.noreply.github.com>
2019-03-29 16:50:10 +0530

    Update case.py

--- api/endpoints/case.py
+++ api/endpoints/case.py
@@ -4,14 +4,14 @@ from endpoints import *
 @app.route('/case/<case_id>', methods=['GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def case_metadata(case_id):
-    case = mgdb_handler.read_all(cases_collection, case_id)
+    case = mgdb_handler.read_all(cases_collection, str(case_id))
     judge_ids = get_metas_to_node(case_id, "case", "judge")
     judges = []
     for id in judge_ids:
-        judges.append(mgdb_handler.read_all(judges_collection, serial=id)[0]["judge_name"])
+        judges.append(mgdb_handler.read_all(judges_collection, serial=int(id))[0])
 
     case["judges"] = judges
-
+    case["year"] = mgdb_handler.read_all(years_collection, name=case["file_name"].split("_")[0])[0]
     return jsonify(case)
 
 
@@ -21,9 +21,9 @@ def case_line_distribution(case_id):
     result = {}
     for i in range (1947 ,2020):
         result[i] = 0
-    subgraph = lkg.query(judges =[],catch=[], keywords=[] , judgements = [], types =[], year_range=[], acts =[])
+    subgraph = lkg
     data = lkg.nodes(data=True)
-    such_cases = subgraph[case_id]
+    such_cases = subgraph.in_edges(["case_" + str(case_id)])
     for case in such_cases:
         all_metas = lkg.in_edges(case)
         for meta, _ in all_metas:
@@ -36,7 +36,7 @@ def case_line_distribution(case_id):
 @app.route('/case/<case_id>/timeline', methods=['GET'])
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def case_timeline(case_id):
-    case_file = mgdb_handler.read_all(cases_collection, serial=case_id)[0]["file_name"]
+    case_file = mgdb_handler.read_all(cases_collection, serial=str(case_id))[0]["file_name"]
 
     result = {item[0]: item[1] for item in get_timelines(case_file, nlp)}
 
@@ -56,17 +56,17 @@ def case_citations(case_id):
 
     act_ids = get_metas_to_node(case_id, "case", "act")
     for id in act_ids:
-        act = mgdb_handler.read_all(acts_collection, serial=id)[0]
+        act = mgdb_handler.read_all(acts_collection, serial=int(id))[0]
         result["cited_acts"].append({act["serial"]: act["name"]})
 
     cited_case_ids = get_metas_to_node(case_id, "case", "case")
     for id in cited_case_ids:
-        cited_case = mgdb_handler.read_all(cases_collection, serial=id)[0]
+        cited_case = mgdb_handler.read_all(cases_collection, serial=str(id))[0]
         result["cited_cases"].append({cited_case["serial"]: cited_case["title"]})
 
     cited_by_cases = get_metas_from_node(case_id, "case", "case")
     for id in cited_by_cases:
-        cited_by_case = mgdb_handler.read_all(cases_collection, serial=id)[0]
+        cited_by_case = mgdb_handler.read_all(cases_collection, serial=str(id))[0]
         result["cited_by_cases"].append({cited_by_case["serial"]: cited_by_case["title"]})
 
     return jsonify(result)
