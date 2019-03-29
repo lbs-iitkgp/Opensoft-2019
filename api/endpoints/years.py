e65b92c734d47f73a600d6fd6fe2ca7f5852b3b2
Pankaj kumar agarwal <43635589+Shankusu993@users.noreply.github.com>
2019-03-29 16:57:56 +0530

    Update years.py

--- api/endpoints/years.py
+++ api/endpoints/years.py
@@ -5,9 +5,6 @@ from endpoints import *
 @cross_origin(origin='localhost', headers=['Content- Type', 'Authorization'])
 def year_metadata(year):
     year = mgdb_handler.read_all(keyword_collection, name=year)[0]
-    number_of_cases = len(get_metas_from_node(year["serial"], "year", "case"))
-    year["number_of_cases"] = number_of_cases
-
     return jsonify(year)
 
 
@@ -16,21 +13,21 @@ def year_metadata(year):
 def year_cases(year):
     result = []
     year = mgdb_handler.read_all(keyword_collection, name=year)[0]
-    case_ids = get_metas_from_node(year["serial"], "year", "case")
+    case_ids = get_metas_from_node(year["serial"], "year", "case", split=False)
     for id in case_ids:
-        case = get_metas_from_node(year["serial"], "year", "case")
-        result.append(case)
+        if id.startswith("case_"):
+            case = mgdb_handler.read_all(cases_collection, serial=str(id))[0]
+            result.append(case)
 
     return jsonify(result)
 
 
 @app.route('year/<year>/piechart',methods=['GET'])
 def year_piechart(year):
-    result = []
-    subgraph = lkg.query(judges = [], subjects=[], keywords=[], judgements = [], types =[], year_range=[year], acts =[])
-    
+    result = [] 
+   
     data = lkg.nodes(data=True)
-    such_cases = subgraph[year]
+    such_cases = lkg[year]
     for case in such_cases:
         all_metas = lkg.in_edges(case)
         for meta, _ in all_metas:
