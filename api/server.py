da0df803c8b5355c679b0c822075035f89c5e1f1
Athitya <athityakumar@gmail.com>
2019-03-29 17:20:08 +0530

    Add more pre-flask deploy changes

--- api/server.py
+++ api/server.py
@@ -1,12 +1,14 @@
 from endpoints import *
 
-# from endpoints.act import *
-# from endpoints.case import *
+from endpoints.act import *
+from endpoints.case import *
 from endpoints.catchword import *
-# from endpoints.judge import *
-# from endpoints.keyword import *
-# from endpoints.search import *
+from endpoints.judge import *
+from endpoints.keyword import *
+from endpoints.search import *
+from endpoints.misc import *
+from endpoints.years import *
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run("0.0.0.0.0" ,port=5000, debug=True)
     
