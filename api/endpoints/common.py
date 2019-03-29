d709f1362a5b2654949229cea2050f275616a028
Pankaj kumar agarwal <43635589+Shankusu993@users.noreply.github.com>
2019-03-29 16:51:18 +0530

    Update common.py

--- api/endpoints/common.py
+++ api/endpoints/common.py
@@ -55,22 +55,26 @@ def provide_metadata(judge,judgement,subject,keyword,year,type,nx_graph):
     query = mongo_db.find({"keyword_id":keyword_id})  
 
 
-def get_metas_to_node(id, node_type, meta_type):
+def get_metas_to_node(id, node_type, meta_type, split = True):
     nodes = lkg.nodes(data=True)
     ids = []
     for meta,_ in lkg.in_edges("{}_{}".format(node_type, id)):
         if nodes[meta]["type"] == meta_type:
-            ids.append(meta.split('_')[1])
-
+            if split:
+                ids.append(meta.split('_')[1])
+            else:
+                ids.append(mets)
     return ids
 
 
-def get_metas_from_node(id, node_type, meta_type):
+def get_metas_from_node(id, node_type, meta_type, split = True):
     nodes = lkg.nodes(data=True)
     ids = []
     for meta,_ in lkg.out_edges("{}_{}".format(node_type, id)):
         if nodes[meta]["type"] == meta_type:
-            ids.append(meta.split('_')[1])
-
+            if split:
+                ids.append(meta.split('_')[1])
+            else:
+                ids.append(meta)
     return ids
 
